e29c8283b9f4520ec63803ba4c0a2a4720534ff1
elppaaa <withbcd@gmail.com>
2019-12-18 21:33:02 +0900

    remove logout Button btnMakers DoubleTap -> logout

--- app/src/main/java/ac/kr/project_20191121/Main.java
+++ app/src/main/java/ac/kr/project_20191121/Main.java
@@ -4,9 +4,14 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.nio.channels.InterruptedByTimeoutException;
 
@@ -14,7 +19,8 @@ import ac.kr.project_20191121.FoodOrder.Menu_Order;
 
 public class Main extends Activity {
     ImageButton btnSeatcheck, btnSeatreservation, btnAddtime, btnMenuorder, btnMakers;
-    Button btnlogout;
+    TextView mainhello;
+    String user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +31,9 @@ public class Main extends Activity {
         btnAddtime = (ImageButton) findViewById(R.id.btnAddtime);
         btnMenuorder = (ImageButton) findViewById(R.id.btnMenuorder);
         btnMakers = (ImageButton) findViewById(R.id.btnMakers);
-        btnlogout = (Button) findViewById(R.id.btnLogout);
+        mainhello = (TextView) findViewById(R.id.mainhello);
+        user = getSharedPreferences("login",MODE_PRIVATE).getString("login_id","");
+        mainhello.setText(user + "님 좋은시간 보내세요.");
         btnSeatcheck.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -55,17 +63,35 @@ public class Main extends Activity {
                 startActivity(intent);
             }
         });
-        btnlogout.setOnClickListener(new View.OnClickListener() {
+
+        //제작자들 더블 클릭하면 로그아웃
+        btnMakers.setOnTouchListener(new View.OnTouchListener() {
+            GestureDetector gd = new GestureDetector(new GestureDetector.SimpleOnGestureListener(){
+
+                @Override
+                public boolean onDoubleTap(MotionEvent e) {
+                    SharedPreferences pref = getSharedPreferences("login", MODE_PRIVATE);
+                    pref.edit().clear().commit();
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+                    acfinish();
+                    return true;
+                }
+
+                @Override
+                public boolean onSingleTapConfirmed(MotionEvent e) {
+                    return super.onSingleTapConfirmed(e);
+                }
+            });
             @Override
-            public void onClick(View view) {
-                SharedPreferences pref = getSharedPreferences("login", MODE_PRIVATE);
-                pref.edit().clear().commit();
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(intent);
-                acfinish();
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                gd.onTouchEvent(motionEvent);
+                return true;
             }
         });
+
+
     }
     void acfinish(){
         finish();
